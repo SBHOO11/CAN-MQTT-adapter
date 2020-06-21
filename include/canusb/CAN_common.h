9d9018ee031c0c29ee31dc7de3926c93416182c9
shibinhoo <shibinhoo@gmail.com>
2020-06-22 00:49:22 +0800

    Add encode, decode macros

--- include/canusb/CAN_common.h
+++ include/canusb/CAN_common.h
@@ -8,11 +8,25 @@
 #include <array>
 
 #define CAN_PAYLOAD_LENGTH 8
+#define LITTLE_ENDIAN_ENABLE 1    // 0 means big endian
+
+
+// Encode-decode macros
+#if (LITTLE_ENDIAN_ENABLE == 1)
+    #define ENCODE_2BYTES_TO_NUM16(b1, b2, dst) ( (dst) = ( (b2) << 8) | (b1))
+	#define ENCODE_4BYTES_TO_NUM32(b1, b2, b3, b4, dst) ( (dst) =  ((b4) << 24) | ( (b3) << 16) | ( (b2) << 8) | (b1))
+	#define DECODE_NUM16_TO_2BYTES(src, b1, b2)  ( ( (b2) = (src) >> 8 ), ( (b1) = (src) & 0x00ff ) )
+	#define DECODE_NUM32_TO_4BYTES(src, b1, b2, b3, b4)  ( ( (b4) = (src) >> 24 ), ( (b3) = (src) >> 16 ), ( (b2) = (src) >> 8 ), ( (b1) = (src) & 0x000000ff ) )
+#else
+    #define ENCODE_2BYTES_TO_NUM16(b1, b2, dst) ( (dst) = ((b1) << 8) | (b2))
+    #define ENCODE_4BYTES_TO_NUM32(b1, b2, b3, b4, dst) ( (dst) =  ((b1) << 24) | ((b2) << 16) | ( (b3) << 8) | (b4) )
+    #define DECODE_NUM16_TO_2BYTES(src, b1, b2)  ( ( (b1) = (src) >> 8 ), ( (b2) = (src) & 0x00ff ) )
+    #define DECODE_NUM32_TO_4BYTES(src, b1, b2, b3, b4)  ( ( (b1) = (src) >> 24 ), ( (b2) = (src) >> 16 ), ( (b3) = (src) >> 8 ), ( (b4) = (src) & 0x000000ff ) )
+#endif
+#endif //CAN_MQTT_ADAPTER_CAN_COMMON_H
 
 class CANData {
 public:
     uint can_id;
     std::array<uint, 8> data;
 };
-
-#endif //CAN_MQTT_ADAPTER_CAN_COMMON_H
--- main.cpp
+++ main.cpp
@@ -10,19 +10,21 @@ int main(int argc, char** argv) {
 
     CANData can_data;
 
-//    while (true) {
-//        obj.read(&can_data);
-//
-//        std::cout << "[ID] " << std::hex << can_data.can_id << " - ";
+    while (true) {
+        obj.read(can_data);
+        uint32_t var;
+        ENCODE_2BYTES_TO_NUM16(can_data.data[0], can_data.data[1], var);
+
+        std::cout << "[ID] " << can_data.can_id << " - " << var << std::endl;
 //        for (const auto &value : can_data.data) {
 //            std::cout << value << " ";
 //        }
 //        std::cout << std::endl;
-//    }
+    }
 
-    can_data.can_id = 15;
-    can_data.data = {1,2,3,4,5,6,7,8};
-    obj.write(can_data);
+//    can_data.can_id = 15;
+//    can_data.data = {1,2,3,4,5,6,7,8};
+//    obj.write(can_data);
 
     obj.close();
 
